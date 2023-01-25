86a64e5bfb0dc701aff7bbead7e9ccd9603f9dc6
Nimesh Patel <nimesh.217@gmail.com>
2023-01-25 09:20:12 +0530

    Add files via upload

---
+++ Ex1-FirstWindows/Ex1-FirstWindows/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim no As Integer
+        Dim cnt As Integer
+        no = 50
+        Dim answer As String
+        answer = vbNullString
+
+        For cnt = 1 To no Step 2
+            answer = answer & cnt & ","
+        Next
+        Label1.Text = answer
+    End Sub
+End Class
