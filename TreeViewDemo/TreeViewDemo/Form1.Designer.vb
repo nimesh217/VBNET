1e385591555604a729150ee5bfdb11bdc2a377ce
Nimesh Patel <nimesh.217@gmail.com>
2023-01-25 09:34:53 +0530

    Add files via upload

---
+++ TreeViewDemo/TreeViewDemo/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Science")
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("BCOM")
+        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("MCOM")
+        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Commerce", New System.Windows.Forms.TreeNode() {TreeNode2, TreeNode3})
+        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("BCA")
+        Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("KBS", New System.Windows.Forms.TreeNode() {TreeNode1, TreeNode4, TreeNode5})
+        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("ORGANIC")
+        Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("INORGANIC")
+        Dim TreeNode9 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("MSc Chemistry", New System.Windows.Forms.TreeNode() {TreeNode7, TreeNode8})
+        Dim TreeNode10 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("MSc", New System.Windows.Forms.TreeNode() {TreeNode9})
+        Dim TreeNode11 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("OTHERS")
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(293, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(265, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(182, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Value"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(225, 111)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 40)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(347, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 40)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Remove"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(471, 111)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 40)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Count"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(225, 205)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode1.Name = "Node1"
+        TreeNode1.Text = "Science"
+        TreeNode2.Name = "Node10"
+        TreeNode2.Text = "BCOM"
+        TreeNode3.Name = "Node11"
+        TreeNode3.Text = "MCOM"
+        TreeNode4.Name = "Node2"
+        TreeNode4.Text = "Commerce"
+        TreeNode5.Name = "Node7"
+        TreeNode5.Text = "BCA"
+        TreeNode6.Name = "Node0"
+        TreeNode6.Text = "KBS"
+        TreeNode7.Name = "Node8"
+        TreeNode7.Text = "ORGANIC"
+        TreeNode8.Name = "Node9"
+        TreeNode8.Text = "INORGANIC"
+        TreeNode9.Name = "Node5"
+        TreeNode9.Text = "MSc Chemistry"
+        TreeNode10.Name = "Node4"
+        TreeNode10.Text = "MSc"
+        TreeNode11.Name = "Node6"
+        TreeNode11.Text = "OTHERS"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode6, TreeNode10, TreeNode11})
+        Me.TreeView1.Size = New System.Drawing.Size(342, 139)
+        Me.TreeView1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(490, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(817, 409)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents Label2 As Label
+End Class
---
+++ TreeViewDemo/TreeViewDemo/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Dim txt As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txt = TextBox1.Text
+
+        Try
+            If (TreeView1.SelectedNode Is Nothing) Then
+                TreeView1.Nodes.Add(txt, txt)
+
+            Else
+                TreeView1.SelectedNode.Nodes.Add(txt, txt)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        TextBox1.Clear()
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TreeView1.SelectedNode.GetNodeCount(True) = 0 Then
+            TreeView1.SelectedNode.Remove()
+        Else
+            MsgBox("YOu cannot remove node with child")
+        End If
+
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+        Label2.Text = TreeView1.SelectedNode.GetNodeCount(False)
+    End Sub
+End Class
