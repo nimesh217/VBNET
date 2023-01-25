8c8167a6015fd82c3b9ba5495ff3cd6e73452dd0
Nimesh Patel <nimesh.217@gmail.com>
2023-01-25 09:21:24 +0530

    Add files via upload

---
+++ Ex02-SumMinMax/Ex02-SumMinMax/MyModule.vb
@@ -0,0 +1,45 @@
+﻿Module MyModule
+    Sub myMethod()
+        Console.Write("hi")
+    End Sub
+
+    Sub Main()
+        Dim a, b, c As Integer
+
+        Console.Write("Enter first value: ")
+        a = Console.ReadLine()
+        Console.Write("Enter second value: ")
+        b = Console.ReadLine()
+        Console.Write("Enter third value: ")
+        c = Console.ReadLine()
+        Dim tot, min, max As Integer
+        Dim avg As Decimal
+
+
+
+        tot = a + b + c
+        avg = tot / 3
+        If a > b And a > c Then
+            max = a
+        ElseIf b > a And b > c Then
+            max = b
+        Else
+            max = c
+        End If
+        If a < b And a < c Then
+            min = a
+        ElseIf b < a And b < c Then
+            min = b
+        Else
+            min = c
+        End If
+
+        Console.WriteLine("The Sum of " + a.ToString() + ", " + b.ToString() + " and " + c.ToString() + " is " + tot.ToString())
+        Console.WriteLine("Average is " + avg.ToString())
+        Console.WriteLine("Maximum Value is " + max.ToString())
+        Console.WriteLine("Minimum value is " + min.ToString())
+        Console.WriteLine("Average is " & avg)
+        Console.ReadKey()
+
+    End Sub
+End Module
---
+++ Ex03-DataTypeConversion/Ex03-DataTypeConversion/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+    Sub Main()
+        Dim n As Integer
+        Dim da As Date
+        Dim bl As Boolean = True
+        n = 1234567
+        da = Today
+        Dim f As Double
+        f = 4556.24513
+        Console.WriteLine(bl)
+        Console.WriteLine(da)
+        Console.WriteLine(n)
+        Console.WriteLine(CStr(bl))
+        Console.WriteLine(CSByte(bl))
+        Console.WriteLine(CStr(da))
+        Console.WriteLine(CChar(CStr(n)))
+        Console.WriteLine(CChar(CStr(da)))
+        Console.WriteLine(f)
+        Console.WriteLine(CStr(f))
+        Console.ReadKey()
+    End Sub
+End Module
