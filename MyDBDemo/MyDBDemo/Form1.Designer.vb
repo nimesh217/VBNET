71d343da36de13d4e913aa5ef05a78d0ddb76790
Nimesh Patel <nimesh.217@gmail.com>
2023-02-14 09:26:19 +0530

    database code

---
+++ MyDBDemo/MyDBDemo/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMobile = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPassword)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtMobile)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtEmail)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(91, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(401, 233)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "UserDetails"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(150, 35)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(188, 26)
+        Me.txtName.TabIndex = 1
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(150, 78)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(188, 26)
+        Me.txtEmail.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "E-Mail"
+        '
+        'txtMobile
+        '
+        Me.txtMobile.Location = New System.Drawing.Point(150, 124)
+        Me.txtMobile.Name = "txtMobile"
+        Me.txtMobile.Size = New System.Drawing.Size(188, 26)
+        Me.txtMobile.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(56, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Mobile no"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(150, 170)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(188, 26)
+        Me.txtPassword.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Password"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(672, 22)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(225, 337)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(57, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Insert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(57, 99)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(123, 45)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(57, 170)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(123, 45)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Cancel"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(57, 237)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(123, 45)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Location = New System.Drawing.Point(91, 286)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(551, 281)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "List of User"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(957, 594)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtMobile As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+End Class
---
+++ MyDBDemo/MyDBDemo/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Imports System.Data.Sql
+
+
+Public Class Form1
+
+#Region "Variables"
+
+    Dim con As OleDbConnection
+    Dim cmd As OleDbCommand
+    Dim dsuser As DataSet
+    Dim adp As OleDbDataAdapter
+
+
+#End Region
+
+#Region "FUNCTIONS"
+
+#End Region
+
+
+#Region "EVENTS"
+
+#End Region
+
+    Public Sub dbconnect()
+        Try
+            con = New OleDbConnection()
+
+            con.ConnectionString = ""
+            con.Open()
+        Catch ex As Exception
+            MessageBox.Show("dbconnect: " + ex.Message.ToString)
+        Finally
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+        End Try
+
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If Button1.Text = "Insert" Then
+                Button1.Text = "Save"
+                Button2.Enabled = False
+                Button3.Enabled = False
+                txtName.Clear()
+                txtEmail.Clear()
+                txtMobile.Clear()
+                txtPassword.Clear()
+                txtName.Focus()
+            Else
+                If (con.State = ConnectionState.Closed) Then
+                    con.Open()
+                    cmd = New OleDbCommand()
+                    cmd.Connection = con
+                    cmd.CommandType = CommandType.Text
+                    cmd.CommandText = "insert into tmuser(name,email,mobile,password)values(@u_name,@u_email,@u_mobile,@u_password)"
+                    cmd.Parameters.Clear()
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text)
+                    cmd.Parameters.AddWithValue("@mobile", txtMobile.Text)
+                    cmd.Parameters.AddWithValue("@password", txtPassword.Text)
+                    If (cmd.ExecuteNonQuery() > 0) Then
+                        MessageBox.Show("recored Inserted")
+
+                        Button1.Text = "Insert"
+                        Button2.Enabled = True
+                        Button3.Enabled = True
+                    End If
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show("insert: " + ex.Message.ToString)
+        Finally
+
+        End Try
+    End Sub
+End Class
